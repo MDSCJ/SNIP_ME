52b9a504d2959d45b192be9220a3af62feea54d0
W.G.I.Adeesha <induruadeesha2356@gmail.com>
2026-03-04 14:52:58 +0530

    Initial Spring Boot project setup and folder structure

---
+++ Backend/src/test/java/com/starc/snipme/SnipmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starc.snipme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
