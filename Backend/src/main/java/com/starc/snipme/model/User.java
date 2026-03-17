facff5b64ba233399b7c907655fc00a04eb4634a
Shavini <shavinikaveesha2004@gmail.com>
2026-03-17 11:34:44 +0530

    refactor: improve base User entity structure

--- Backend/src/main/java/com/starc/snipme/model/User.java
+++ Backend/src/main/java/com/starc/snipme/model/User.java
@@ -1,6 +1,17 @@
 package com.starc.snipme.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +28,6 @@ public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(unique = true, nullable = false)
-    private String username;
     
     @Column(unique = true, nullable = false)
     private String email;
